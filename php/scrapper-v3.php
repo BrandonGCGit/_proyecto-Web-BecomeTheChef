ce7c536b876cd4b572da747f3ec135e0776755ae
DelgaSol <you@example.com>
2022-11-21 17:52:26 -0600

    x

--- php/scrapper-v3.php
+++ php/scrapper-v3.php
@@ -16,7 +16,48 @@
     //f($_POST){
 
         $links =["https://www.bonviveur.es/recetas/ensaladilla-rusa-casera",
-        "https://www.bonviveur.es/recetas/tortilla-francesa"
+        "https://www.bonviveur.es/recetas/tortilla-francesa",
+        "https://www.bonviveur.es/recetas/sandwich-mixto",
+        "https://www.bonviveur.es/recetas/huevos-fritos",
+        "https://www.bonviveur.es/recetas/patatas-importancia",
+        "https://www.bonviveur.es/recetas/caballa-con-tomate",
+        "https://www.bonviveur.es/recetas/tarta-pina",
+        "https://www.bonviveur.es/recetas/souffle-de-chocolate",
+        "https://www.bonviveur.es/recetas/tortitas-de-platano",
+        "https://www.bonviveur.es/recetas/caldo-dashi",
+        "https://www.bonviveur.es/recetas/fingers-de-queso",
+        "https://www.bonviveur.es/recetas/gambas-a-la-gabardina",
+        "https://www.bonviveur.es/recetas/helado-de-leche-condensada",
+        "https://www.bonviveur.es/recetas/batido-de-sandia",
+        "https://www.bonviveur.es/recetas/pure-de-zanahoria",
+        "https://www.bonviveur.es/recetas/provolone-al-horno",
+        "https://www.bonviveur.es/recetas/batido-de-melon",
+        "https://www.bonviveur.es/recetas/flan-de-platano",
+        "https://www.bonviveur.es/recetas/batido-de-fresa-y-platano",
+        "https://www.bonviveur.es/recetas/dorada-a-la-plancha",
+        "https://www.bonviveur.es/recetas/salmon-a-la-plancha",
+        "https://www.bonviveur.es/recetas/fritos-de-rape-o-de-pixin",
+        "https://www.bonviveur.es/recetas/muslos-de-pollo-al-horno",
+        "https://www.bonviveur.es/recetas/uvas-rellenas-de-queso",
+        "https://www.bonviveur.es/recetas/tarta-de-yogur-griego",
+        "https://www.bonviveur.es/recetas/solomillo-de-ternera-a-la-plancha",
+        "https://www.bonviveur.es/recetas/crema-de-espinacas",
+        "https://www.bonviveur.es/recetas/gelatina-de-limon",
+        "https://www.bonviveur.es/recetas/guisantes-con-jamon",
+        "https://www.bonviveur.es/recetas/peras-al-vino-tinto",
+        "https://www.bonviveur.es/recetas/ensalada-de-macarrones-fria",
+        "https://www.bonviveur.es/recetas/sunomono",
+        "https://www.bonviveur.es/recetas/patatas-chips-fritas",
+        "https://www.bonviveur.es/recetas/tarta-de-queso-y-limon-fria-sin-horno",
+        "https://www.bonviveur.es/recetas/tarta-de-moka-y-galletas",
+        "https://www.bonviveur.es/recetas/bocadillo-almussafes",
+        "https://www.bonviveur.es/recetas/limonada",
+        "https://www.bonviveur.es/recetas/patacones-tostones",
+        "https://www.bonviveur.es/recetas/esparragos-trigueros-a-la-plancha",
+        "https://www.bonviveur.es/recetas/ensalada-de-pepino",
+        "https://www.bonviveur.es/recetas/ensalada-de-tomate",
+        "https://www.bonviveur.es/recetas/crema-de-aguacate",
+        "https://www.bonviveur.es/recetas/huevos-fritos-con-farinato"
         ];
 
         
@@ -43,13 +84,17 @@
             $data['name'] = $detailed_recipe->find('h1',0)->plaintext;
             
             //IMG DE LA RECETA
-            //$finalimg
+            $finalimg ="";
             $image = $detailed_recipe->find('.single__image img',0);
             if($image == null){
                 $data['image'] = "no image";
             }else {
                 $data['image'] = $image->src;
-                file_put_contents("./images/recipe-".generateRandomString().".png",file_get_contents($image->src));
+
+               //file_put_contents("./images/recipe-".generateRandomString().".png",file_get_contents($image->src));
+               $finalimg = "recipe-".generateRandomString();
+               move_uploaded_file("./images/".$finalimg);
+               file_put_contents("./images/".$finalimg.".png",file_get_contents($image->src));
             }
 
 
@@ -145,10 +190,10 @@
                 //                    "occasion_id" =>  1,
                                     "occasion_id" =>  "1",
                 //                    "complexity_id" => 1,
-                                    "complexity_id" =>  "1"
+                                    "complexity_id" =>  "1",
                 //                    "likes_id" => 1,
                 //                    "likes_id" => (int)$_POST["recipe_likes"],
-                //                    "recipes_img" => "IMG"
+                                    "recipes_img" => $finalimg
                                     //"recipes_img" => $img
                                 ]);
             $recipe[] = $data;
